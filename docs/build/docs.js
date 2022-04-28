f842a531aaa053ff5b553bc99365bc641313f725
Stephen <stephband@cruncher.ch>
2022-04-28 20:21:28 +0200

    Builds docs to docs/build

--- docs/build/docs.js
+++ docs/build/docs.js
@@ -1,7 +1,7 @@
 /* <slide-show> 
    2.0.0
    By Stephen Band
-   Built 2022-04-28 18:10 */
+   Built 2022-04-28 18:18 */
 
 import"https://stephen.band/bolt/attributes/data-toggleable.js";import"https://stephen.band/bolt/classes/device.js";import"https://stephen.band/bolt/classes/toggle-block.js";
 //# sourceMappingURL=docs.js.map
